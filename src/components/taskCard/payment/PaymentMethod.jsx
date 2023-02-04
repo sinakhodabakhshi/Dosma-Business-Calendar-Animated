36d5e84453ff1d7188b5d3af6c89f0127b0cd711
sina <skhodabakhshi77@gmail.com>
2023-02-04 18:45:35 +0100

    remove a bug

--- src/components/taskCard/payment/PaymentMethod.jsx
+++ src/components/taskCard/payment/PaymentMethod.jsx
@@ -1,7 +1,7 @@
 export default function PaymentMethod({ paymentMethod }) {
   return (
-    <motion.p className="text-end mt-5 font-semibold text-red-400 ">
+    <p className="text-end mt-5 font-semibold text-red-400 ">
       {paymentMethod}
-    </motion.p>
+    </p>
   );
 }
